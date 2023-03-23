791bd8ef6d172a2c4803cb013a403f6b8a82d2f2
Pedroxzz <joaopedroicm2117@gmail.com>
2023-03-23 20:27:05 -0300

    first commit

---
+++ 5-Periodo/POO/Atividades/src/Atividade1.java
@@ -0,0 +1,17 @@
+
+public class Atividade1 {
+    public static void main(String[] args) {
+        
+        int n1 = 0, n2 = 0, n3 = 0, n4 = 0;
+
+        int soma = n1 + n2 + n3 + n4;
+        int sub = n1 - n2 - n3 - n4;
+        int mult = n1 * n2 * n3 * n4;
+        int div = n1 / n2 / n3 / n4;
+        int media = soma / 4;
+        
+        System.out.println(); 
+
+    }
+    
+}
