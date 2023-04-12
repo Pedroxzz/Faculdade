a6a8fa0533ce319881dd38eb2435756c8cc64fbb
Joao <joaopedroicm2117@gmail.com>
2023-04-12 20:12:19 -0300

    12-04 pt1

---
+++ 5-Periodo/POO/ ExemplodeClasses/src/exemplodeclasses/ContaPoupanca.java
@@ -0,0 +1,12 @@
+package exemplodeclasses;
+
+public class ContaPoupanca extends Conta{
+    String datasAniversarios;
+
+    @Override
+    void saqueConta(double valor){
+        if((saldo - valor) <0 )
+            System.out.println("ERRO: Saldo ficará abaixo de R$ 0.00");
+        else saldo = saldo -valor;
+    }
+}
--- 5-Periodo/POO/ ExemplodeClasses/src/exemplodeclasses/TesteExemploClasseConta.java
+++ 5-Periodo/POO/ ExemplodeClasses/src/exemplodeclasses/TesteExemploClasseConta.java
@@ -2,10 +2,12 @@ package exemplodeclasses;
 
 public class TesteExemploClasseConta {
     public static void main(String[] args) {
+        ContaPoupanca cp = new ContaPoupanca();
+        cp.saqueConta(30.00);
+        System.out.println("Saldo em Poupança: " +cp.saldo);
 
-        Conta contaObj = new Conta();
-        contaObj.saqueConta(500);
-
-        System.out.println("O valor do saldo é: "+contaObj.saldo);
+        ContaCorrente cr = new ContaCorrente();
+        cr.saqueConta(500.00);
+        System.out.println("Saldo em Conta Corrente: " +cr.saldo);
     }
 }
