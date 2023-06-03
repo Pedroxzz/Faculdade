3cb4cf62bda33dac3c12cb26404ef79e72f14d29
João Pedro Soares <joaopedroicm2117@gmail.com>
2023-06-02 22:38:16 -0300

    .

--- 5-Periodo/POO/ExemplodeFormulario/src/exemplodeformulario/CadastrodeProdutos.java
+++ 5-Periodo/POO/ExemplodeFormulario/src/exemplodeformulario/CadastrodeProdutos.java
@@ -1,6 +1,8 @@
 
 package exemplodeformulario;
 
+import javax.swing.JOptionPane;
+
 public class CadastrodeProdutos extends javax.swing.JFrame {
 
     public CadastrodeProdutos() {
@@ -12,7 +14,15 @@ public class CadastrodeProdutos extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabelTitulo = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
+        jLabelCodigo = new javax.swing.JLabel();
+        jLabelValor = new javax.swing.JLabel();
+        jLabelDescricao = new javax.swing.JLabel();
+        jTextFieldDescricao = new javax.swing.JTextField();
+        jTextFieldCodigo = new javax.swing.JTextField();
+        jTextFieldValor = new javax.swing.JTextField();
+        jButtonGravar = new javax.swing.JButton();
+        jButtonLimpar = new javax.swing.JButton();
+        jButtonSair = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -20,7 +30,44 @@ public class CadastrodeProdutos extends javax.swing.JFrame {
         jLabelTitulo.setForeground(new java.awt.Color(0, 102, 255));
         jLabelTitulo.setText("Cadastro de Produtos");
 
-        jLabel1.setText("Código:");
+        jLabelCodigo.setFont(new java.awt.Font("Dialog", 2, 18)); // NOI18N
+        jLabelCodigo.setText("Código:");
+
+        jLabelValor.setFont(new java.awt.Font("Dialog", 2, 18)); // NOI18N
+        jLabelValor.setText("Valor:");
+
+        jLabelDescricao.setFont(new java.awt.Font("Dialog", 2, 18)); // NOI18N
+        jLabelDescricao.setText("Descrição:");
+
+        jTextFieldDescricao.setFont(new java.awt.Font("Dialog", 2, 18)); // NOI18N
+
+        jTextFieldCodigo.setFont(new java.awt.Font("Dialog", 2, 18)); // NOI18N
+
+        jTextFieldValor.setFont(new java.awt.Font("Dialog", 2, 18)); // NOI18N
+
+        jButtonGravar.setFont(new java.awt.Font("Dialog", 3, 14)); // NOI18N
+        jButtonGravar.setText("Gravar");
+        jButtonGravar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonGravarActionPerformed(evt);
+            }
+        });
+
+        jButtonLimpar.setFont(new java.awt.Font("Dialog", 3, 14)); // NOI18N
+        jButtonLimpar.setText("Limpar");
+        jButtonLimpar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonLimparActionPerformed(evt);
+            }
+        });
+
+        jButtonSair.setFont(new java.awt.Font("Dialog", 3, 14)); // NOI18N
+        jButtonSair.setText("Sair");
+        jButtonSair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonSairActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -29,25 +76,81 @@ public class CadastrodeProdutos extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(90, 90, 90)
+                        .addGap(98, 98, 98)
                         .addComponent(jLabelTitulo))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(31, 31, 31)
-                        .addComponent(jLabel1)))
-                .addContainerGap(105, Short.MAX_VALUE))
+                        .addGap(40, 40, 40)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabelValor)
+                                .addGap(56, 56, 56)
+                                .addComponent(jTextFieldValor, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jButtonLimpar)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(jLabelDescricao)
+                                        .addComponent(jLabelCodigo)))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(18, 18, 18)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(jTextFieldCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(jTextFieldDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addGap(33, 33, 33)
+                                        .addComponent(jButtonGravar)
+                                        .addGap(37, 37, 37)
+                                        .addComponent(jButtonSair, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)))))))
+                .addContainerGap(97, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(39, 39, 39)
                 .addComponent(jLabelTitulo)
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabelCodigo)
+                    .addComponent(jTextFieldCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addComponent(jLabel1)
-                .addContainerGap(227, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabelDescricao)
+                    .addComponent(jTextFieldDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabelValor)
+                    .addComponent(jTextFieldValor, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 67, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButtonGravar)
+                    .addComponent(jButtonSair)
+                    .addComponent(jButtonLimpar))
+                .addGap(32, 32, 32))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButtonGravarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonGravarActionPerformed
+        Produto p = new Produto();
+        p.codigo = Integer.parseInt(jTextFieldCodigo.getText());
+        p.descricao = jTextFieldDescricao.getText();
+        p.valor = Double.parseDouble(jTextFieldCodigo.getText());
+        JOptionPane.showMessageDialog(null,"Os dados digitados no formulário "
+                + "são: "+"\n"+p.codigo+"\n"+p.descricao+"\n"+p.valor+"\n");
+    }//GEN-LAST:event_jButtonGravarActionPerformed
+
+    private void jButtonLimparActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonLimparActionPerformed
+        jTextFieldCodigo.setText("");
+        jTextFieldValor.setText("");
+        jTextFieldDescricao.setText("");
+        jTextFieldCodigo.requestFocus();
+    }//GEN-LAST:event_jButtonLimparActionPerformed
+
+    private void jButtonSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSairActionPerformed
+        dispose();
+    }//GEN-LAST:event_jButtonSairActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -81,7 +184,15 @@ public class CadastrodeProdutos extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton jButtonGravar;
+    private javax.swing.JButton jButtonLimpar;
+    private javax.swing.JButton jButtonSair;
+    private javax.swing.JLabel jLabelCodigo;
+    private javax.swing.JLabel jLabelDescricao;
     private javax.swing.JLabel jLabelTitulo;
+    private javax.swing.JLabel jLabelValor;
+    private javax.swing.JTextField jTextFieldCodigo;
+    private javax.swing.JTextField jTextFieldDescricao;
+    private javax.swing.JTextField jTextFieldValor;
     // End of variables declaration//GEN-END:variables
 }
